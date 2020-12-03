d0600192833ca98005a77a19eed6a2d7c674543e
Rishabh chauhan <64160163+Rishabhchauhan30@users.noreply.github.com>
2020-12-03 18:03:16 +0530

    Update Stock Span Problem

--- 11.Stack and Queues/StockSpanProblem.cpp
+++ 11.Stack and Queues/StockSpanProblem.cpp
@@ -48,3 +48,14 @@ int main(){
     }
 return 0;
 }
+
+Time Complexity: O(n). It seems more than O(n) at first look. 
+    If we take a closer look, we can observe that every element of the array is added and removed from the stack at most once. 
+    So there are total 2n operations at most. 
+    Assuming that a stack operation takes O(1) time, we can say that the time complexity is O(n).
+
+Auxiliary Space: O(n) in worst case when all elements are sorted in decreasing order.
+    
+-----------------------------------------------------------------------------------------------------------------------------------------------------------------
+Another approach can be without using stack Else everything is same.
+ 
